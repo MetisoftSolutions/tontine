08e9bbc29bcd3bb3d8de1a30137c6384345ae80b
James Brawn, Jr <james.brawn.jr@gmail.com>
2018-03-31 14:52:55 -0500

    Pool directory.

--- contracts/tontineContractDirectory.sol
+++ contracts/tontineContractDirectory.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.16;
 
 contract TontineContractDirectory {
     address public owner;
-    mapping(string => address) public contractName2Address;
+    mapping(bytes32 => address) public contractName2Address;
     
     
     
@@ -12,7 +12,7 @@ contract TontineContractDirectory {
     
     
     
-    function updateContract(string name, address addr) {
+    function updateContract(bytes32 name, address addr) {
         require(owner == msg.sender);
         contractName2Address[name] = addr;
     }
