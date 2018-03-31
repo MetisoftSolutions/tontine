6f38028f3da0f4d9a89dd6fe3cabaa2e0cfcb300
James Brawn, Jr <james.brawn.jr@gmail.com>
2018-03-31 10:17:48 -0500

    Adding initial tontinePool user reg. Removing scaffold contracts.

---
+++ contracts/tontinePool.sol
@@ -0,0 +1,64 @@
+pragma solidity ^0.4.16;
+
+contract TontinePool {
+    
+    address[] public participants;
+    address owner;
+    bool useRandomOrdering = false;
+    bool isParticipantRegistrationClosed = false;
+    
+    
+    
+    function TontinePool(bool _useRandomOrdering) public {
+        owner = msg.sender;
+        useRandomOrdering = _useRandomOrdering;
+    }
+    
+    
+    
+    modifier requireOwner() {
+        require(owner == msg.sender);
+        _;
+    }
+    
+    
+    
+    function addParticipant(address participant) public requireOwner {
+        require(!isParticipantRegistrationClosed);
+        participants.push(participant);
+    }
+    
+    
+    
+    function removeParticipant(address participant) public requireOwner {
+        require(!isParticipantRegistrationClosed);
+        uint indexToRemove;
+        bool found = false;
+        
+        for (uint i = 0; i < participants.length; i++) {
+            if (participants[i] == participant) {
+                indexToRemove = i;
+                found = true;
+                break;
+            }    
+        }
+        
+        if (found) {
+            participants[indexToRemove] = participants[participants.length - 1];
+            participants.length -= 1;
+        }
+    }
+    
+    
+    
+    function getNumberOfParticipants() public constant returns (uint) {
+        return participants.length;
+    }
+    
+    
+    
+    function closeRegistration() public requireOwner() {
+        isParticipantRegistrationClosed = true;
+    }
+    
+}
